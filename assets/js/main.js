d44ca5428b43e86be1e82b22310dfcc2b28174e9
mdmunnaahmned <msdmunna77@gmail.com>
2023-12-23 18:41:00 +0600

    image animation --done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,37 @@ window.addEventListener("scroll", function () {
     }
 });
 
-$(function () {
-    $(".selectpicker").selectpicker();
-});
+
+// On Scroll image Fade in transition js
+  function isInViewport(element, offset) {
+    var rect = element.getBoundingClientRect();
+    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    return (
+      (rect.top + offset) <= windowHeight &&
+      (rect.bottom - offset) >= 0
+    );
+  }
+
+  function handleScroll() {
+    var images = document.querySelectorAll('.fadeIn');
+
+    images.forEach(function(image) {
+      if (isInViewport(image, 100)) {
+        setTimeout(function() {
+            image.classList.add('fadeInAnimation');
+        }, 150);
+      }
+    });
+
+    var fadeUp = document.querySelectorAll('.fadeUp')
+    fadeUp.forEach(function(item) {
+        var delay = parseInt(item.getAttribute('data-animation-delay')) || 0;
+      if (isInViewport(item, 100)) {
+        setTimeout(function() {
+            item.classList.add('fadeUpAnimation');
+        }, delay);
+      }
+    });
+  }
+  window.addEventListener('scroll', handleScroll);
+  handleScroll();
