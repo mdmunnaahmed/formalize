806eb21134692f7e1a8a695f2acea2b4ac8edb7a
mdmunnaahmned <msdmunna77@gmail.com>
2023-12-30 23:52:09 +0600

    side section --start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ["./dist/*{html,js}"],
+  content: ["./dist/*.{html,js}"],
+  darkMode: "media",
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1440px",
+    },
+    colors: {
+      base: {
+        default: "#0019fd",
+        light: "#0772FF",
+        dark: "#041E60",
+      },
+      title: {
+        default: "#0F172A",
+      },
+      body: {
+        default: "#0F172A",
+      },
+      white: {
+        default: "#fff",
+      },
+      gray: {
+        light: "#F1F5F9",
+        medium: "#3E3E3E",
+      },
+      dark: {
+        default: "#000",
+      },
+    },
+    borderColor: {
+      border: {
+        default: "#D9D9D9",
+        thumb: "#bbd4f5",
+      },
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
+    extend: {
+      colors: {
+        base: {
+          400: "#0019fd",
+        },
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.5rem",
+        "2xl": "2rem",
+        "3xl": "2.225rem",
+        "4xl": "3rem",
+        "5xl": "3.5",
+      },
+    },
   },
   plugins: [],
-}
+};
